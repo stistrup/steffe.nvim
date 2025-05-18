74207f314e3e0fdaf3d44a3dd31492f6588b52a9
Stefan Stistrup <sstistrup@gmail.com>
2025-05-18 14:26:57 +0200

    add leader + o

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -12,6 +12,9 @@ function M.setup()
   -- Swap $ and ¤ for easier end-of-line navigation
   vim.keymap.set({ 'n', 'v', 'i', 'o' }, '$', '¤', {})
   vim.keymap.set({ 'n', 'v', 'i', 'o' }, '¤', '$', { desc = 'Go to end of line' })
+
+  vim.keymap.set({ 'n' }, '<leader>o', 'o<Esc>', { desc = 'Create new line (below)' })
+  vim.keymap.set({ 'n' }, '<leader>O', 'O<Esc>', { desc = 'Create new line (above)' })
 end
 
 return M
