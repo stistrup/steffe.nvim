06e6e8f0c8123732c6f09a1d2fafc2fba7769339
Stefan Stistrup <sstistrup@gmail.com>
2025-11-16 21:10:32 +0100

    add autopairs, comments, surround and fix nvim tree

--- lua/stistrup/plugins/nvim-tree.lua
+++ lua/stistrup/plugins/nvim-tree.lua
@@ -3,12 +3,44 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     local nvimtree = require("nvim-tree")
+    local api = require("nvim-tree.api")
 
     -- recommended settings from nvim-tree documentation
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
+    -- Custom on_attach function for keymaps
+    local function on_attach(bufnr)
+      local opts = { buffer = bufnr }
+      
+      -- Load default mappings first
+      api.config.mappings.default_on_attach(bufnr)
+      
+      -- Custom h/l navigation
+      local lefty = function()
+        local node = api.tree.get_node_under_cursor()
+        if (node.name == ".." or node.nodes) and node.open then
+          api.node.open.edit()
+        else
+          api.node.navigate.parent()
+        end
+      end
+      
+      local righty = function()
+        local node = api.tree.get_node_under_cursor()
+        if (node.name == ".." or node.nodes) and not node.open then
+          api.node.open.edit()
+        end
+      end
+      
+      vim.keymap.set("n", "h", lefty, opts)
+      vim.keymap.set("n", "<Left>", lefty, opts)
+      vim.keymap.set("n", "l", righty, opts)
+      vim.keymap.set("n", "<Right>", righty, opts)
+    end
+
     nvimtree.setup({
+      on_attach = on_attach,
       view = {
         width = 50,
         relativenumber = true,
@@ -21,8 +53,8 @@ return {
         icons = {
           glyphs = {
             folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
+              arrow_closed = "", -- arrow when folder is closed
+              arrow_open = "", -- arrow when folder is open
             },
           },
         },
@@ -47,10 +79,9 @@ return {
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
-
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
   end,
 }
